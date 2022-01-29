cc280eacafc02698a51a7941a6efc90f1ae23ac5
Devdeep <75716586+Devdeep-J-S@users.noreply.github.com>
2022-01-29 09:20:23 +0530

    Delete EventManagar directory

--- EventManagar/Event/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Eventdata
-
-class Event_register_form(forms.ModelForm):
-    class Meta:
-        model = Eventdata
-        fields = ("__all__")
-        exclude = ()   
